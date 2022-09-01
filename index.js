b3fc92a42c906f16341f3129bf9c5d9c0fa64c00
Davi Marcilio <davimarcilio.js@gmail.com>
2022-09-01 15:18:11 -0300

    bugs fixes

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const questions = [
     'Quantas pessoas eu ajudei hoje?'
 ]
 const ask = (index = 0) => {
-    process.stdout.write(questions[index] + '\n\n')
+    process.stdout.write("\n\n" + questions[index] + ' : > ')
 }
 
 ask();
-
+const answers = [];
 process.stdin.on('data', data => {
-    process.stdout.write(data.toString().trim() + '\n')
+    answers.push(data.toString().trim());
+    if (answers.length < questions.length) {
+        ask(answers.length);
+    } else {
+        console.log(answers);
+        process.exit();
+    }
+
 })
