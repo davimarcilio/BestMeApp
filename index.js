41a7e6e1e7066df7dd579566c1313c3b412b99dd
Davi Marcilio <davimarcilio.js@gmail.com>
2022-09-01 15:27:50 -0300

    Final

--- index.js
+++ index.js
@@ -15,8 +15,24 @@ process.stdin.on('data', data => {
     if (answers.length < questions.length) {
         ask(answers.length);
     } else {
-        console.log(answers);
         process.exit();
     }
+})
+process.on('exit', () => {
+    console.log(`
+    Bacana Davi!
 
+    O que você aprendeu hoje foi:
+    ${answers[0]}
+
+    O que te aborreceu e você poderia melhorar foi:
+    ${answers[1]}
+
+    O que te deixou feliz hoje:
+    ${answers[2]}
+
+    Você ajudou ${answers[3]} pessoas hoje!!
+
+    Volte amanhã para novas reflexões
+    `);
 })
